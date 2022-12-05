5f8f7a32e95b568c5f42ef28f780417cdecc77e4
phemza <Flexyski1999@Gmail.com>
2022-12-05 03:46:57 -0800

    array and object

---
+++ main.js
@@ -0,0 +1,38 @@
+let users=[
+    {name:'sunday', age: 12, address:'Osogbo'},
+    {name:'sodiq', age: 120, address:'Osogbo'},
+    {name:'Emmanuel', age: 100, address:'Ibadan'},
+    {name:'Blessing', age: 300, address:'Akure'},
+    {name:'Opeyemi', age: 400, address:'Lagos'},
+    {name:'Femi', age: 1000, address:'Ondo'},
+
+]
+document.getElementById("name1").innerHTML = (users[0].name)
+document.getElementById("age1").innerHTML = (users[0].age)
+document.getElementById("address1").innerHTML = (users[0].address)
+
+
+document.getElementById("name2").innerHTML = (users[1].name)
+document.getElementById("age2").innerHTML = (users[1].age)
+document.getElementById("address2").innerHTML = (users[1].address)
+
+
+document.getElementById("name3").innerHTML = (users[2].name)
+document.getElementById("age3").innerHTML = (users[2].age)
+document.getElementById("address3").innerHTML = (users[2].address)
+
+
+document.getElementById("name4").innerHTML = (users[3].name)
+document.getElementById("age4").innerHTML = (users[3].age)
+document.getElementById("address4").innerHTML = (users[3].address)
+
+
+document.getElementById("name5").innerHTML = (users[4].name)
+document.getElementById("age5").innerHTML = (users[4].age)
+document.getElementById("address5").innerHTML = (users[4].address)
+
+
+document.getElementById("name6").innerHTML = (users[5].name)
+document.getElementById("age6").innerHTML = (users[5].age)
+document.getElementById("address6").innerHTML = (users[5].address)
+
